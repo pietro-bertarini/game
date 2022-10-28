7de22da4ed2df28d53061ee77ac97140750a6b83
Pietro Bertarini <pietrobcmota@gmail.com>
2022-10-28 13:07:50 -0300

    testCamera .

--- Assets/JumpGame/Scripts/JumpCameraFollow.cs
+++ Assets/JumpGame/Scripts/JumpCameraFollow.cs
@@ -31,7 +31,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class CameraFollow : MonoBehaviour 
+public class JumpCameraFollow : MonoBehaviour 
 {
     public GameObject targetObject;
     private float distanceToTarget;
